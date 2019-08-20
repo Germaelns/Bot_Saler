fb45079700c33713ae26aba9e8b8335e24c6c48b
germael <nikolay.shevchenko.work@gmail.com>
2019-08-20 15:10:30 +0300

    Bll: update VKService

--- BLL/Services/VKService.py
+++ BLL/Services/VKService.py
@@ -1,5 +1,6 @@
 import re
 import vk
+import requests
 from DAL.Repositories.GroupRepository import GroupRepository
 
 
@@ -12,13 +13,25 @@ class VKService:
     def get_data_from_vk(self):
         session = vk.Session(access_token=self.user.vk_token)
         api = vk.API(session)
+        group_repo = GroupRepository(self.session)
 
         post_data = list()
 
-        last_post_time = float(self.user.last_post_time)
-
-        groups = GroupRepository(self.session)
-
-        for group in groups.get_all_groups(self.user.id):
+        for group in group_repo.get_all_groups(self.user.id):
 
             response = api.wall.get(owner_id=group.vk_group_id, v=5.74, count=5)
+
+            for item in response['items']:
+                if self.user.last_post_time < item['date']:
+                    if item['text'] is not "" and item['text'].count('http') == 1:
+                        try:
+                            url = re.search("(?P<url>https?://[^\s]+)", item["text"]).group("url")
+                            response = requests.get(url)
+                            url = re.findall(r'<meta property="og:url" content=[\'"]?([^\'" >]+)', str(response.content))
+                            ali_item_id = url[0].split(".html")[0].split("/item/")[1]
+                            text = item['text'].replace(url, '')
+                            post_data.append([text, ali_item_id])
+                        except:
+                            pass
+
+        return post_data
