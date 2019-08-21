e6d79870283d031b3d9d76f1cdb7dab2f4189ec8
germael <nikolay.shevchenko.work@gmail.com>
2019-08-21 16:14:19 +0300

    BLL: Now EPNService can create all deeplinks from one group with one request

--- BLL/Services/EPNService.py
+++ BLL/Services/EPNService.py
@@ -8,40 +8,63 @@ class EPNService:
         self.user = user
         self.group = group
 
+    @staticmethod
+    def build_request_data(user, vk_items: list):
+
+        data = {
+            "user_api_key": "" + user.epn_api_token,
+            "user_hash": "" + user.epn_hash,
+            "api_version": 1,
+            "requests": {
+
+            }
+        }
+
+        counter = 0
+
+        for item in vk_items:
+            data["requests"]["rq_" + str(counter)] = {
+                "action": "offer_info",
+                "lang": "ru",
+                "id": "" + item[1],
+                "currency": "USD"
+
+            }
+
+            counter = + 1
+
+        return data
+
+    @staticmethod
+    def send_request(user, vk_items):
+
+        data = EPNService.build_request_data(user, vk_items)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        return requests.post("http://api.epn.bz/json", data=json.dumps(data), headers=headers)
+
     def create_deeplinks(self, vk_items: list):
+
+        responce = EPNService.send_request(self.user, vk_items)
 
         deeplinks = list()
 
-        for item in vk_items:
-            data = {
-                "user_api_key": "" + self.user.epn_api_token,
-                "user_hash": "" + self.user.epn_hash,
-                "api_version": 1,
-                "requests": {
-                    "request_1": {
-                        "action": "offer_info",
-                        "lang": "ru",
-                        "id": "" + item[1],
-                        "currency": "USD"
-
-                    }
-                }
-            }
-            headers = {
-                'Content-Type': 'application/json'
-            }
-            responce = requests.post("http://api.epn.bz/json", data=json.dumps(data), headers=headers)
+        counter = 0
+        for item in responce.json()["results"]:
 
             deeplink = {
-                "image": responce.json()["results"]["request_1"]["offer"]["picture"],
-                "title": item[0],
-                "url": responce.json()["results"]["request_1"]["offer"]["url"],
-                "price": responce.json()["results"]["request_1"]["offer"]["sale_price"],
-                "sale": int((responce.json()["results"]["request_1"]["offer"]["sale_price"] / responce.json()["results"]["request_1"]["offer"]["price"]) * 100),
+                "image": responce.json()["results"]["rq_" + str(counter)]["offer"]["picture"],
+                "title": vk_items[counter][0],
+                "url": responce.json()["results"]["rq_" + str(counter)]["offer"]["url"],
+                "price": responce.json()["results"]["rq_" + str(counter)]["offer"]["sale_price"],
+                "sale": int((responce.json()["results"]["rq_" + str(counter)]["offer"]["sale_price"] / responce.json()["results"]["rq_" + str(counter)]["offer"]["price"]) * 100),
                 "group_id": self.group.id,
                 "user_id": self.group.user_id
             }
 
             deeplinks.append(deeplink)
+            counter = + 1
 
         return deeplinks
