397e3dee42ff4dee4649a87ed29839067f2edf46
germael <nikolay.shevchenko.work@gmail.com>
2019-08-22 13:32:06 +0300

    BLL: Refactor VKService and created TGService

---
+++ BLL/Services/TGService.py
@@ -0,0 +1,23 @@
+import telebot
+from telebot import types
+
+
+class TGService:
+
+    def __init__(self, user, bot_token, post_data: dict):
+        self.user = user
+        self.bot_token = bot_token
+        self.post_data = post_data
+
+    def post_to_channel(self):
+        bot = telebot.TeleBot(self.bot_token)
+
+        markup = types.InlineKeyboardMarkup()
+        btn_my_site = types.InlineKeyboardButton(text='Заказать', url=self.post_data["url"])
+        markup.add(btn_my_site)
+
+        bot.send_photo(chat_id=self.user.tg_channel, photo=self.post_data[""],
+                       caption=f"\n {self.post_data['title']}\n\nЦена: {self.post_data['price']}\n"
+                               f"Скидка: {str(self.post_data['sale'])}%", reply_markup=markup)
+
+        return 0
--- BLL/Services/VKService.py
+++ BLL/Services/VKService.py
@@ -15,23 +15,26 @@ class VKService:
         api = vk.API(session)
         group_repo = GroupRepository(self.session)
 
-        post_data = list()
+        vk_items = list()
 
         for group in group_repo.get_all_groups(self.user.id):
 
             response = api.wall.get(owner_id=group.vk_group_id, v=5.74, count=5)
 
+            print(response['items'][0])
+
             for item in response['items']:
-                if self.user.last_post_time < item['date']:
+                if int(self.user.last_post_time) < item['date']:
                     if item['text'] is not "" and item['text'].count('http') == 1:
                         try:
                             url = re.search("(?P<url>https?://[^\s]+)", item["text"]).group("url")
+                            text = item['text'].replace(url, '')
                             response = requests.get(url)
                             url = re.findall(r'<meta property="og:url" content=[\'"]?([^\'" >]+)', str(response.content))
-                            ali_item_id = url[0].split(".html")[0].split("/item/")[1]
-                            text = item['text'].replace(url, '')
-                            post_data.append([text, ali_item_id])
+                            if url[0]:
+                                ali_item_id = url[0].split(".html")[0].split("/item/")[1]
+                                vk_items.append([text, ali_item_id])
                         except:
                             pass
 
-        return post_data
+        return vk_items
